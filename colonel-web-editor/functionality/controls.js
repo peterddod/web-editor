8f8b2dd14d5d0a394de13202abe7082953d628cd
Peter Dodd <peterdodd01@gmail.com>
2021-04-27 21:19:52 +0100

    Closes #6, each character in editor encompassed by span to find position when clicked

--- colonel-web-editor/functionality/controls.js
+++ colonel-web-editor/functionality/controls.js
@@ -1,72 +1,66 @@
 var editor = document.getElementById('editor');
+var textSection = document.getElementById('textSection');
 
-document.addEventListener('keydown', function (e) {  
-  e.preventDefault();
-  e.stopPropagation();
+var active = null;
 
-  if (e.key == 'Tab') 
-  {
-    addChar('\t');
-  }
-  else if (e.key == 'Delete') 
-  {
-    delChar(caretPosition + 1);
-  }
-  else if (e.key == 'Enter') 
-  {
-    addChar('\n');
-  }
-  else if (e.key == 'Backspace') 
-  {
-    delChar(caretPosition);
-    caretPosition -= 1;
-  }
-  else if (e.key == 'ArrowLeft') 
-  {
-    caretPosition -= 1;
-  }
-  else if (e.key == 'ArrowRight') 
-  {
-    caretPosition += 1;
-  }
-  else if (e.key.length == 1)
-  {
-    addChar(e.key);
-  }
-
-  if (caretPosition < 0)
-  {
-    caretPosition = 0;
-  }
-  else if (caretPosition > textValue.length)
-  {
-    caretPosition = textValue.length;
-  }
-
-  editor.innerText = textValue.substring(0, caretPosition) + '|' + textValue.substring(caretPosition, textValue.length);
+document.addEventListener('click', function (e) 
+{
+  active = e.target;
+  spanText();
 })
 
-window.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.editor').forEach(el => {
-      let characters = el['innerText'].split('');
-      el.innerHTML = '';
-      characters.forEach(char => {
-          let span = document.createElement('span');
-          span.innerText = char;
-          span.addEventListener('click', function () {
-              let position = 0;
-              let el = this;
-              while (el.previousSibling !== null) {
-                  position++;
-                  el = el.previousSibling;
-              }
-              caretPosition = position;
-              editor.innerHTML = textValue.substring(0, caretPosition) + '|' + textValue.substring(caretPosition, textValue.length);
-          });
-          el.appendChild(span);
-      });
-  });
-});
+document.addEventListener('keydown', function (e) 
+{  
+  if (active == editor)
+  {
+    e.preventDefault();
+    e.stopPropagation();
+  
+    if (e.key == 'Tab') 
+    {
+      addChar('\t');
+    }
+    else if (e.key == 'Delete') 
+    {
+      delChar(caretPosition + 1);
+    }
+    else if (e.key == 'Enter') 
+    {
+      addChar('\n');
+    }
+    else if (e.key == 'Backspace') 
+    {
+      delChar(caretPosition);
+      caretPosition -= 1;
+    }
+    else if (e.key == 'ArrowLeft') 
+    {
+      caretPosition -= 1;
+    }
+    else if (e.key == 'ArrowRight') 
+    {
+      caretPosition += 1;
+    }
+    else if (e.key.length == 1)
+    {
+      addChar(e.key);
+    }
+  
+    if (caretPosition < 0)
+    {
+      caretPosition = 0;
+    }
+    else if (caretPosition > textValue.length)
+    {
+      caretPosition = textValue.length;
+    }
+  
+    //editor.innerText = textValue.substring(0, caretPosition) + '|' + textValue.substring(caretPosition, textValue.length);
+
+    spanText();
+  }
+  
+})
 
 function addChar(char)
 {
@@ -77,4 +71,52 @@ function addChar(char)
 function delChar(pos)
 {
   textValue = textValue.substring(0, pos - 1) + textValue.substring(pos, textValue.length);
+}
+
+function spanText()
+{
+  editor.innerHTML = "";
+  let characters = textValue.split('');
+  var count = 1;
+  let p = document.createElement('p');
+  p.setAttribute('class', 'newline_p');
+  editor.appendChild(p);
+
+  characters.forEach(char => {
+    let span = document.createElement('span');
+    if (count == caretPosition && active == editor) 
+    {
+      span.innerHTML = char + '|';
+    }
+    else
+    {
+      span.innerHTML = char;
+    }
+    span.setAttribute('style', 'white-space: pre;');
+
+    span.addEventListener('mousedown', function () 
+    {
+      let position = 0;      
+      let el = this;
+      while (el.previousSibling !== null) {
+              position++;
+              el = el.previousSibling;
+      }
+      caretPosition = position;
+      spanText();
+    });
+    p.appendChild(span);
+    if (char == '\n')
+    {
+      let p = document.createElement('p');
+      editor.appendChild(p);
+    }
+
+    count += 1;
+  })
+
+  if (count == 1) 
+  {
+    editor.innerText = '|';
+  }
 }
